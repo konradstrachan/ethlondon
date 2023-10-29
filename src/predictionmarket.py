a534b6b8084c093b8d62960d6a9b3fc1a57a8484
Konrad <konrad.strachan@gmail.com>
2023-10-29 10:26:08 +0000

    Updated function descriptions and added more assertions for logic checking

--- src/predictionmarket.py
+++ src/predictionmarket.py
@@ -4,8 +4,18 @@ import smartpy as sp
 def main():
     class MatchaPredictionMarket(sp.Contract):
         """
-        XXXX
+        Prediction market that allows anyone to bet on the outcome of an event.
 
+        For example: 
+            Someone can propose that "Biden will with the US Election in 2024"
+            The prediction market could open immediately and be set to end just
+            before the outcome is announced on 5 November 2024
+
+            Anyone can place a bet either that the claim is true or false until
+            that time. Once the end time is reached, the Prediction Market will
+            Assert the outcome using the Optimistic Oracle and use the despute
+            mechanism to achieve finality on the statement. At this point any
+            bet that matches the winning outcome will be paid out
         """
                     
         def __init__(self):
@@ -14,14 +24,25 @@ def main():
         @sp.entry_point
         def propose_claim(self, prediction, start_time, end_time):
             """
-            XXXX
+            Create a new claim that creates a market that others can speculate on. 
+            All claims must evaluate to Yes / No (True/False) statements which 
+            can then be evaluated based on fact.
+
+            e.g. The max temperature in London on 29/10/23 will be 19C
 
             Args:
                 prediction (string): name of market
             """
+            # Make sure the claim hasn't already been proposed
             assert not self.data.claims.contains(prediction)
-
+            # Sanity check timining
             assert start_time < end_time
+            # Can't create a claim that can't become active
+            assert sp.now < end_time
+            # Prevent spam by requiring the proposer to initialise the
+            # pool with a small amount of funds asserting the positive
+            # outcome
+            assert sp.amount >= sp.mutez(100)
 
             self.data.claims[prediction] = sp.record(
                 start_time = start_time,
@@ -30,14 +51,15 @@ def main():
                 finalised = False,
                 outcome = False,
                 bets = {},
-                poolTrue = 0,
+                poolTrue = sp.amount,
                 poolFalse = 0
             )
 
         @sp.entry_point
         def place_bet(self, prediction, outcome):
             """
-            XXXX
+            Enables anyone to place a bet on a prediction market currently active
+            either betting the outcome is positive (true) or negative (false)
 
             Args:
                 prediction (string): name of market
@@ -51,8 +73,9 @@ def main():
             assert claim.end_time > sp.now 
             # Market has not yet finished
             assert claim.resolved == False
-            # Bet amount must be greater than 0
-            assert sp.amount > 0
+            # Bet amount must be greater than a small base amount
+            # to prevent spam and inflated compute cost on payout
+            assert sp.amount > sp.mutez(10)
 
             predicted_outcome = sp.cast(bool, outcome)
 
@@ -67,7 +90,11 @@ def main():
         @sp.entry_point
         def close_market_and_assert_outcome(self, prediction):
             """
-            XXXX
+            Once the end time has passed the market will go into a despute
+            resolution mode whereby the claim that has been bet against will
+            be proposed to the Optimistic Oracle. This starts the process of
+            challenging / disputing the claim which enables the final result
+            to be stored and the winning betters to be paid
 
             Args:
                 prediction (string): name of market
@@ -93,7 +120,9 @@ def main():
         @sp.entry_point
         def finalise_outcome(self, prediction):
             """
-            XXXX
+            Once the end time has passed the market and the claim has finished
+            the challenge / dispute claim window, this will finalise the outcome
+            and pay the winners
 
             Args:
                 prediction (string): name of market
